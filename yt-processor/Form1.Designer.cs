57de89b536fc2a2efbfae003a359383571487767
vhateg <62607065+vhateg@users.noreply.github.com>
2020-03-24 20:59:19 +0200

    Add files via upload

---
+++ yt-processor/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace yt_processor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.thumbnail = new System.Windows.Forms.PictureBox();
+            this.yturl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.verify = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.thumbnail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // thumbnail
+            // 
+            this.thumbnail.Location = new System.Drawing.Point(12, 12);
+            this.thumbnail.Name = "thumbnail";
+            this.thumbnail.Size = new System.Drawing.Size(236, 133);
+            this.thumbnail.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.thumbnail.TabIndex = 0;
+            this.thumbnail.TabStop = false;
+            // 
+            // yturl
+            // 
+            this.yturl.Location = new System.Drawing.Point(254, 28);
+            this.yturl.Name = "yturl";
+            this.yturl.Size = new System.Drawing.Size(264, 20);
+            this.yturl.TabIndex = 1;
+            this.yturl.TextChanged += new System.EventHandler(this.yturl_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insert URL here:";
+            // 
+            // verify
+            // 
+            this.verify.Location = new System.Drawing.Point(436, 54);
+            this.verify.Name = "verify";
+            this.verify.Size = new System.Drawing.Size(82, 23);
+            this.verify.TabIndex = 3;
+            this.verify.Text = "Download";
+            this.verify.UseVisualStyleBackColor = true;
+            this.verify.Click += new System.EventHandler(this.verify_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(254, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(436, 122);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(82, 23);
+            this.Browse.TabIndex = 5;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Verify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Save Path:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 160);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.verify);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yturl);
+            this.Controls.Add(this.thumbnail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "YB Processor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.thumbnail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox thumbnail;
+        private System.Windows.Forms.TextBox yturl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button verify;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ yt-processor/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Web;
+using System.Diagnostics;
+
+namespace yt_processor
+{
+    public partial class Form1 : Form
+    {
+        String img_id;
+        String path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "";
+        }
+
+        private void verify_Click(object sender, EventArgs e)
+        {
+            var url = yturl.Text;
+            var uri = new Uri(url);
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            var vid_id = string.Empty;
+            if (query.AllKeys.Contains("v"))
+            {
+                vid_id = query["v"];
+            }
+            else
+            {
+                vid_id = uri.Segments.Last();
+            }
+            img_id = "http://img.youtube.com/vi/" + vid_id + "/maxresdefault.jpg";
+            thumbnail.Load(img_id);
+            if (path != "")
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = @"youtube-dl.exe";
+                string args = " -f bestaudio" + " -o " + @"dl\output.webm" + " " + url;
+                startInfo.Arguments = args;
+                label1.Text = args;
+                Process.Start(startInfo);
+            }
+            else
+                MessageBox.Show("Choose a path!");
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                path = folderBrowserDialog1.SelectedPath;
+                path = '\u0022' + path + @"\output.webm" + '\u0022';
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            path = textBox1.Text;
+            path = '\u0022' + path + @"\output.webm" + '\u0022';
+        }
+
+        private void yturl_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var url = yturl.Text;
+            var uri = new Uri(url);
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            var vid_id = string.Empty;
+            if (query.AllKeys.Contains("v"))
+            {
+                vid_id = query["v"];
+            }
+            else
+            {
+                vid_id = uri.Segments.Last();
+            }
+            img_id = "http://img.youtube.com/vi/" + vid_id + "/maxresdefault.jpg";
+            thumbnail.Load(img_id);
+        }
+
+    }
+}
